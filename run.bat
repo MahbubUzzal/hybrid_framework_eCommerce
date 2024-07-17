eb12502589e050beec6d575b9d5f879bedf67e8d
Mahbub Uzzal <mahbubur7@gmail.com>
2024-07-17 22:20:54 +0100

    another commit

--- run.bat
+++ run.bat
@@ -5,9 +5,9 @@ cd /d "%~dp0"
 :: Uncomment and adjust the path to your virtual environment
 C:\Users\mahbu\PycharmProjects\hybrid_framework\venv\Scripts\activate
 
-:: Option 2: Ensure Python and pytest are in the PATH
+:: Option 2: Ensure Python and pytest are in the PATH when you are not using virtual environment
 :: Adjust the paths to your Python installation and Scripts directory
-set PATH=C:\Users\mahbu\AppData\Local\Programs\Python\Python311;C:\Users\mahbu\AppData\Local\Programs\Python\Python311\Scripts;%PATH%
+::set PATH=C:\Users\mahbu\AppData\Local\Programs\Python\Python311;C:\Users\mahbu\AppData\Local\Programs\Python\Python311\Scripts;%PATH%
 
 :: Run pytest with arguments
 pytest -v -s -m "regression" --html=../reports/report.html test_cases/ --browser chrome
