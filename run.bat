d81185b0b024fa17eacb874030e6f31b44de5ec1
Mahbub Uzzal <mahbubur7@gmail.com>
2024-07-17 22:32:02 +0100

    updating bat

--- run.bat
+++ run.bat
@@ -3,8 +3,7 @@ cd /d "%~dp0"
 
 :: Option 1: Activate the virtual environment
 :: Uncomment and adjust the path to your virtual environment
-C:\Users\mahbu\PycharmProjects\hybrid_framework\venv\Scripts\activate
-
+C:\Users\mahbu\PycharmProjects\hybrid_framework\.venv\Scripts\activate
 :: Option 2: Ensure Python and pytest are in the PATH when you are not using virtual environment
 :: Adjust the paths to your Python installation and Scripts directory
 ::set PATH=C:\Users\mahbu\AppData\Local\Programs\Python\Python311;C:\Users\mahbu\AppData\Local\Programs\Python\Python311\Scripts;%PATH%
